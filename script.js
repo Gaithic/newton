5a57f5efdce8d18c9defcf0aba10c7f67f09706f
Rohit gautam <rohitgautamofficialmail@gmail.com>
2022-06-05 20:58:00 +0530

    hello

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var divHide = document.getElementById('expense');
 var expense = document.getElementById('expenseAdd');
 var field = document.getElementById('field');
 var expensive = document.getElementById('expensive');
+var display = document.getElementById('display');
 
 
 income.onclick = function(){
@@ -14,11 +15,19 @@ expensive.onclick = function(){
     document.getElementById("expense").style.visibility = "visible";
 }
 
+
+display.onclick = function(){
+    document.getElementById("spanResult").style.visibility = "visible";
+}
+
+
 add.addEventListener('click', function(){
     var personIncome = document.getElementById('personIncome').value;
     var personSalary = document.getElementById('personSalary').value;
     // console.log(personIncome  + ', ' + personSalary);
     alert(personIncome  + ', ' +  personSalary);
+    var result = personIncome + ', ' + personSalary;
+    document.getElementById('spanResult').textContent = result;
 })
 
 expense.addEventListener('click', function(){
@@ -26,6 +35,8 @@ expense.addEventListener('click', function(){
     var expenseSalary = document.getElementById('expenseMoney').value;
     // console.log(expenseIncome, expenseSalary);
     alert(expenseIncome  + ', ' + expenseSalary);
+    var result1 = expenseIncome + ', ' + expenseSalary;
+    document.getElementById('spanResult').textContent = result1;
 })
 
 
